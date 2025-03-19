81d5a025ead5c789066273584b7479b4d0f7758a
otmn <pat.othman.ly@gmail.com>
2025-03-19 15:58:32 +0100

    Code version finale

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,39 +1,36 @@
+import java.util.OptionalDouble;
 import java.util.Scanner;
 
 public class Calculatrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double nombre1 = demanderNombre(scanner, "Entrez le premier nombre : ", false);
-        char operateur = getValidOperator(scanner);
-        double nombre2 = demanderNombre(scanner, "Entrez le deuxième nombre : ", operateur == '/');
+        double nombre1 = demanderNombre(scanner, "Entrez le premier nombre : ");
+        char operateur = demanderOperateur(scanner);
+        double nombre2 = demanderNombre(scanner, "Entrez le deuxième nombre : ");
 
-        double resultat = calculer(nombre1, nombre2, operateur);
-        System.out.println("Résultat : " + nombre1 + " " + operateur + " " + nombre2 + " = " + resultat);
+        OptionalDouble resultat = calculer(nombre1, nombre2, operateur);
+
+        if (resultat.isPresent()) {
+            System.out.println("Résultat : " + nombre1 + " " + operateur + " " + nombre2 + " = " + resultat.getAsDouble());
+        } else {
+            System.out.println("Erreur : Impossible d'effectuer l'opération.");
+        }
 
         scanner.close();
     }
 
-    // Méthode unique pour demander un nombre (évite la duplication du code)
-    private static double demanderNombre(Scanner scanner, String message, boolean verifierDivisionParZero) {
-        double nombre;
-        while (true) {
+    private static double demanderNombre(Scanner scanner, String message) {
+        System.out.print(message);
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Erreur : Veuillez entrer un nombre valide !");
+            scanner.next();
             System.out.print(message);
-            if (scanner.hasNextDouble()) {
-                nombre = scanner.nextDouble();
-                if (!verifierDivisionParZero || nombre != 0) {
-                    return nombre;
-                }
-                System.out.println("Erreur : Division par zéro impossible !");
-            } else {
-                System.out.println("Erreur : Veuillez entrer un nombre valide !");
-                scanner.next(); // Efface l’entrée incorrecte
-            }
         }
+        return scanner.nextDouble();
     }
 
-    // Optimisation avec une seule vérification regex pour l'opérateur
-    private static char getValidOperator(Scanner scanner) {
+    private static char demanderOperateur(Scanner scanner) {
         System.out.print("Entrez un opérateur (+, -, *, /) : ");
         while (!scanner.hasNext("[-+*/]")) {
             System.out.println("Erreur : Opérateur invalide !");
@@ -43,14 +40,14 @@ public class Calculatrice {
         return scanner.next().charAt(0);
     }
 
-    // Calcul simplifié
-    private static double calculer(double nombre1, double nombre2, char operateur) {
+    private static OptionalDouble calculer(double nombre1, double nombre2, char operateur) {
         return switch (operateur) {
-            case '+' -> nombre1 + nombre2;
-            case '-' -> nombre1 - nombre2;
-            case '*' -> nombre1 * nombre2;
-            case '/' -> nombre1 / nombre2;
-            default -> Double.NaN; // Ne devrait jamais arriver
+            case '+' -> OptionalDouble.of(nombre1 + nombre2);
+            case '-' -> OptionalDouble.of(nombre1 - nombre2);
+            case '*' -> OptionalDouble.of(nombre1 * nombre2);
+            case '/' -> (nombre2 != 0) ? OptionalDouble.of(nombre1 / nombre2) : OptionalDouble.empty();
+            default -> OptionalDouble.empty();
         };
     }
 }
+
